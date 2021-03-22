e5250846de7e4f10197c6fbf6f48daa092ffab87
John Vorwald <cricketjohnny@gmail.com>
2021-03-21 19:36:24 -0500

    moving postgres over to new repo.

---
+++ db/grocery-db-init.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+RUN_INIT_POP="psql -X -U docker --set ON_ERROR_STOP=on --set AUTOCOMMIT=off grocery"
+
+$RUN_INIT_POP <<SQL
+  CREATE TABLE IF NOT EXISTS inventory (
+    id SERIAL PRIMARY KEY,
+    itemName varchar(150) NOT NULL,
+    itemPrice money NOT NULL
+  );
+  INSERT INTO inventory(itemName, itemPrice)
+  VALUES
+    ('apple', 0.55),
+    ('banana', 0.60),
+    ('orange', 0.45),
+    ('cheese', 0.75),
+    ('bread', 1.25),
+    ('tomato', 0.30),
+    ('lettuce', 0.75),
+    ('orange juice', 2.50);
+  COMMIT;
+SQL
