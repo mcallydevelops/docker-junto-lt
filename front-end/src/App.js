1435dc2a7628c690e3d23b2bb13d9a99f1ec010f
John Vorwald <cricketjohnny@gmail.com>
2021-03-23 22:21:10 -0500

    updating docker image. just using the simple web server built into the create-react-app.

---
+++ front-end/src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  state = {
+    notList: {},
+    theList: {},
+    conditionals: {
+      isError: false,
+      isCLicked: false
+    }
+  }
+
+  theClick = () => {
+    fetch('localhost:8081/endpoint')
+    .then(
+      response => {
+        this.setState(prevState => (
+          {
+            theList: [...prevState.theList, response.json()]
+          }
+        ))
+      })
+  }
+
+  render() {
+    return <div className="App">
+      <div className="the-button">
+        <button onClick={this.theClick}>Click Me</button>
+        { this.state.theList.length > 0 &&
+        <ul>
+          { this.state.theList.map( item => (
+            <li key={item.id}>{item.name}</li>
+          )) }
+        </ul>
+        } 
+      </div>
+    </div>
+  };
+}
+
+export default App;
